a552224f5457c9b7cee846a5cc83353544ba2a08
Jordyvan <jordy.van@student.umn.ac.id>
2020-12-02 08:13:41 +0700

    munculin event schedule

--- src/app/schedule/schedule.page.ts
+++ src/app/schedule/schedule.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {EventSrvcService} from '../services/event-srvc.service';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-schedule',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.page.scss'],
 })
 export class SchedulePage implements OnInit {
-
-  constructor() { }
+  event: any;
+  constructor(private eventSrvc: EventSrvcService,) { }
 
   ngOnInit() {
+    this.eventSrvc.getAll().snapshotChanges().pipe(
+        map(changes =>
+            changes.map(c => ({key: c.payload.key, ...c.payload.val()})))
+    ).subscribe( data => {
+      this.event = data;
+    });
   }
 
 }
